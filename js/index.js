8531e7346dc671c462c555768183eb50e3e56f52
gastond23 <gaston.dandre@gmail.com>
2021-03-26 21:54:46 -0300

    body main finish

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 let menuBtn = document.getElementById('menu-btn');
 let toggleMenu = true;
 let menuList = document.getElementById('mn-list');
+let body = document.getElementsByClassName('body');
 
 
 menuBtn.addEventListener('click', () => {
     if (toggleMenu) {
         menuBtn.classList.add('is-active');
         menuList.classList.add('menu-active');
+        body[0].classList.add('fix');
         toggleMenu = false;
     } else {
         menuBtn.classList.remove('is-active');
         menuList.classList.remove('menu-active');
+        body[0].classList.remove('fix');
         toggleMenu = true;
     }
 })
